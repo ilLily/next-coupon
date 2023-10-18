c2b4a3b386f7473c30326477489d94a559427d1e
Lily Kuo <ilwitulily0209@gmail.com>
2023-10-18 08:37:50 +0800

    style guide set up

--- components/layout/navbar.js
+++ components/layout/navbar.js
@@ -1,5 +1,12 @@
 import React from 'react'
+import style from './navbar.module.css'
 
 export default function Navbar() {
-  return <div>Navbar</div>
+  return (
+    <>
+      <nav>
+        <div className={style.nav}>COUPON AROUND</div>
+      </nav>
+    </>
+  )
 }
---
+++ pages/coupon/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from '@/components/layout/navbar'
+
+export default function coupon() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <div>index</div>
+    </>
+  )
+}
